6971dd94b069a07ddc843166f31536d2817b3d2e
sumitsrk25 <sumitkalyana25@gmail.com>
2019-10-07 03:31:51 +0530

    added slider

--- src/components/Wizard/components/StepperCarousal/index.tsx
+++ src/components/Wizard/components/StepperCarousal/index.tsx
@@ -4,44 +4,57 @@ import { Col } from "react-bootstrap";
 
 import styles from "./style.module.css";
 
-export class StepperCarousal extends React.Component {
+export class StepperCarousal extends React.Component<any> {
   render() {
     var settings = {
       slidesToShow: 4,
       slidesToScroll: 3
     };
     return (
-      <Col className="py-4">
+      <div className="py-4 px-4">
         <Slider {...settings}>
           <Items
             src="https://d3e02gns9oqhhr.cloudfront.net/marketing/widget/Icons-17-05-17-GZ/10.svg"
             label="Property"
+            {...this.props}
           />
           <Items
             src="https://d3e02gns9oqhhr.cloudfront.net/marketing/widget/Icons-17-05-17-GZ/10.svg"
             label="House"
+            {...this.props}
           />
           <Items
             src="https://d3e02gns9oqhhr.cloudfront.net/marketing/widget/Icons-17-05-17-GZ/10.svg"
             label="Flat"
+            {...this.props}
           />
           <Items
             src="https://d3e02gns9oqhhr.cloudfront.net/marketing/widget/Icons-17-05-17-GZ/10.svg"
             label="Business"
+            {...this.props}
           />
           <Items
             src="https://d3e02gns9oqhhr.cloudfront.net/marketing/widget/Icons-17-05-17-GZ/10.svg"
             label="Storage"
+            {...this.props}
           />
         </Slider>
-      </Col>
+      </div>
     );
   }
 }
 
-export const Items = ({ src, label }: { src: string; label: string }) => {
+export const Items = ({
+  src,
+  label,
+  onSelect = () => {}
+}: {
+  src: string;
+  label: string;
+  onSelect?: () => void;
+}) => {
   return (
-    <div className={styles.items_card}>
+    <div onClick={onSelect} className={styles.items_card}>
       <img className={styles.items_card__img} alt={label} src={src} />
       <p className="mb-0">{label}</p>
     </div>
--- src/components/Wizard/components/steps/Landing.tsx
+++ src/components/Wizard/components/steps/Landing.tsx
@@ -1,31 +1,120 @@
 import React from "react";
-import { Col, Row, Button } from "react-bootstrap";
+import { Col, Row, Button, Carousel, Form } from "react-bootstrap";
 import { StepFooter } from "../footer";
 import { StepperCarousal } from "../StepperCarousal";
 
 export const LandingStep = () => {
+  const [index, setIndex] = React.useState(0);
+
   return (
     <>
       <Row className="mx-0">
-        <Col>
-          <p className="-o-font-title text-center">
-            What is value of your property
-          </p>
-          <p className="-o-font-title text-center" style={{ fontSize: "20px" }}>
-            100% <span className="-o-color-primary">Free</span> Get Rated
-          </p>
-          <p className="-o-font-subtitle text-center">
-            Which property would you like to rate?
-          </p>
-        </Col>
+        {index === 0 ? (
+          <Col>
+            <p className="-o-font-title text-center">
+              What is value of your property
+            </p>
+            <p
+              className="-o-font-title text-center"
+              style={{ fontSize: "20px" }}
+            >
+              100% <span className="-o-color-primary">Free</span> Get Rated
+            </p>
+            <p className="-o-font-subtitle text-center">
+              Which property would you like to rate?
+            </p>
+          </Col>
+        ) : (
+          <Col>
+            <p className="-o-font-title text-center">
+              What is value of your property
+            </p>
+          </Col>
+        )}
       </Row>
       <Row className="mx-0 flex-grow-1 justify-content-center">
-        <StepperCarousal />
-        <Col className="flex-grow flex-shrink mb-1">
-          <Button className="px-5">Request a rating now</Button>
+        <Col>
+          <Carousel activeIndex={index} controls={false} indicators={false}>
+            <Carousel.Item>
+              <StepperCarousal onSelect={() => setIndex(2)} />
+              <div>
+                <Button
+                  className="px-5"
+                  onClick={() => {
+                    if (index) {
+                      setIndex(index - 1);
+                    } else {
+                      setIndex(1);
+                    }
+                  }}
+                >
+                  {index ? "Back" : "Request Rating"}
+                </Button>
+              </div>
+            </Carousel.Item>
+            <Carousel.Item>
+              <StepperCarousal onSelect={() => setIndex(2)} />
+              <div>
+                <Button
+                  className="px-5"
+                  onClick={() => {
+                    if (index) {
+                      setIndex(index - 1);
+                    } else {
+                      setIndex(1);
+                    }
+                  }}
+                >
+                  {index ? "Back" : "Request Rating"}
+                </Button>
+              </div>
+            </Carousel.Item>
+            <Carousel.Item>
+              <div>
+                <RangeCard />
+                <Button
+                  className="px-5"
+                  onClick={() => {
+                    if (index) {
+                      setIndex(index - 1);
+                    } else {
+                      setIndex(1);
+                    }
+                  }}
+                >
+                  {index ? "Back" : "Request Rating"}
+                </Button>
+              </div>
+            </Carousel.Item>
+          </Carousel>
         </Col>
       </Row>
       <StepFooter />
     </>
+  );
+};
+
+const RangeCard = () => {
+  const [value, setValue] = React.useState("100" as string);
+  const onChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <Form.Group>
+      <Form.Control
+        type="range"
+        value={value}
+        onChange={onChange}
+      ></Form.Control>
+      <Form.Group>
+        <Form.Label>How Much area does Property have?</Form.Label>
+        <Form.Control
+          type="number"
+          value={value}
+          onChange={onChange}
+        ></Form.Control>
+      </Form.Group>
+    </Form.Group>
   );
 };
